0f3bd544e4c0df7391efc49c4b86bf391414f73e
sns-nexthink <sanish.kumar@nexthink.com>
2024-02-02 00:18:28 +0530

    json prettify and toggle mode

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,21 @@ export const App: FC<{ name: string }> = ({ name }) => {
   const [encodedStr, SetEncodedStr] = useState<string>()
   const [decodedStr, SetDecodedStr] = useState<string>()
   const [errorMsg, setErrorMsg] = useState<string>()
+  const [json, setJSON] = useState(true)
   const onEncode = () => {
     setErrorMsg('')
     try{
-      const compressedStr = LZUTF8.compress((JSON.stringify(JSON.parse(decodedStr))), {
-        outputEncoding: 'Base64',
-      })
+      let compressedStr = ''
+      if(json){
+        compressedStr = LZUTF8.compress((JSON.stringify(JSON.parse(decodedStr))), {
+          outputEncoding: 'Base64',
+        })
+      } else {
+        compressedStr = LZUTF8.compress((decodedStr), {
+          outputEncoding: 'Base64',
+        })
+      }
+      
       SetEncodedStr(compressedStr)
     }catch(e){
       console.error(e)
@@ -22,10 +31,18 @@ export const App: FC<{ name: string }> = ({ name }) => {
   const onDecode = () => {
     setErrorMsg('')
     try{
-      const decompressedStr = LZUTF8.decompress(encodedStr, {
-        inputEncoding: 'Base64',
-      })
-      SetDecodedStr(JSON.stringify(JSON.parse(decompressedStr), undefined, 4))
+      let decompressedStr = ''
+      if(json){
+        decompressedStr = JSON.stringify(JSON.parse(LZUTF8.decompress(encodedStr, {
+          inputEncoding: 'Base64',
+        })), undefined, 4)
+      } else {
+        decompressedStr = LZUTF8.decompress(encodedStr, {
+          inputEncoding: 'Base64',
+        })
+      }
+      
+      SetDecodedStr(decompressedStr)
     }catch(e){
       console.error(e)
       setErrorMsg(`Decoding Error: ${e.message}`)
@@ -37,8 +54,15 @@ export const App: FC<{ name: string }> = ({ name }) => {
       <div className="playground">
         <textarea value={decodedStr} onChange={e => SetDecodedStr(e.target.value)} />
         <div className="action-buttons">
-          <button onClick={onEncode}>Encode &mdash;&rarr;</button>
-          <button onClick={onDecode}>&larr;&mdash; Decode</button>
+          <button onClick={onEncode}>Encode {json ? 'JSON' : null} &mdash;&rarr;</button>
+          <button onClick={onDecode}>&larr;&mdash; Decode {json ? 'JSON' : null}</button>
+          <div className='switch-container'>
+            <span>JSON: </span>
+            <label className="switch">
+              <input type="checkbox" checked={json} onChange={e => setJSON(!json)} />
+              <span className="slider round"></span>
+            </label>
+          </div>
         </div>
         <textarea value={encodedStr} onChange={e => SetEncodedStr(e.target.value)} />
       </div>
